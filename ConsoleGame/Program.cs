7f31237fc9404251b792e2a38a767d4d4569fb72
arokkeksze <arkosi.armand@blathy.info>
2023-09-27 10:31:00 +0200

    First commit

---
+++ ConsoleGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
