04465f01740ff99347fb796aaf0885e050ee3aa0
arokkeksze <arkosi.armand@blathy.info>
2023-09-27 11:21:05 +0200

    Fixed game breaking when going out of bounds

--- ConsoleGame/Program.cs
+++ ConsoleGame/Program.cs
@@ -20,6 +20,9 @@
 
             bool isInGame = true;
 
+            // Console.WriteLine(Console.WindowWidth);
+            // Console.WriteLine(Console.WindowHeight);
+            
             while (isInGame)
             {
                 Console.Clear();
@@ -34,38 +37,72 @@
                 Console.CursorTop = o_y;
                 Console.Write("O");
 
+
+
+
+
+
+
+
                 switch (Console.ReadKey(true).Key)
                 {
                     case ConsoleKey.LeftArrow:
-                        x_x--;
+                        if (x_x > 0)
+                        {
+                            x_x--;
+                        }
                         break;
 
                     case ConsoleKey.RightArrow:
-                        x_x++;
+                        if (x_x < Console.WindowWidth-1)
+                        {
+                            x_x++;
+                        }
                         break;
 
                     case ConsoleKey.UpArrow:
-                        x_y--;
+                        if (x_y > 1)
+                        {
+                            x_y--;
+                        }
                         break;
 
                     case ConsoleKey.DownArrow:
-                        x_y++;
+                        if (x_y < Console.WindowHeight)
+                        {
+                            x_y++;
+                        }
                         break;
 
                     case ConsoleKey.A:
-                        o_x--;
+                        if (o_x > 0)
+                        {
+                            o_x--;
+                        }
                         break;
 
                     case ConsoleKey.D:
-                        o_x++;
+                        if (o_x < Console.WindowWidth - 1)
+                        {
+                            o_x++;
+                        }
                         break;
 
                     case ConsoleKey.W:
-                        o_y--;
+                        if (o_y > 1)
+                        {
+                            o_y--;
+                        }
                         break;
 
                     case ConsoleKey.S:
-                        o_y++;
+                        if (o_y < Console.WindowHeight)
+                        {
+                            o_y++;
+                        }
+                        break;
+                    case ConsoleKey.End:
+                        isInGame = false;
                         break;
                 }
 
