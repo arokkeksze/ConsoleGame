65de031eecf4209f1cc3656e804b522c10b51f9f
arokkeksze <arkosi.armand@blathy.info>
2023-09-27 10:39:44 +0200

    X and O spawn point

--- ConsoleGame/Program.cs
+++ ConsoleGame/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+             * X, O karakterek
+             * X - W-A-S-D karakterekkel mozgatható
+             * O - bal-jobb-fel-le karakterekkel mozgatható
+             * Ha találkoznak game over
+             */
+
+            //kezdő helyek
+            int x_x = 10;
+            int x_y = 10;
+
+            int o_x = Console.WindowWidth -10;
+            int o_y = Console.WindowHeight -10;
+
+            while (true)
+            {
+                Console.Clear();
+
+                // X
+                Console.CursorLeft = x_x;
+                Console.CursorTop = x_y;
+                Console.Write("X");
+
+                // O
+                Console.CursorLeft = o_x;
+                Console.CursorTop = o_y;
+                Console.Write("O");
+
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        x_x--;
+                        break;
+                }
+            }
         }
     }
 }
